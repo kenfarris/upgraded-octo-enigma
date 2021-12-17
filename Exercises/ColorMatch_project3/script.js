11187a2dbf30229f4e5d63b11eda1e2bfa4572d5
kenfarris <khf2112@columbia.edu>
2021-12-17 00:04:38 -0500

    update

--- Exercises/ColorMatch_project3/script.js
+++ Exercises/ColorMatch_project3/script.js
@@ -4,19 +4,26 @@ function getRandomInt(min=0, max=17 ) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var colorString = Array (0,0,0);
-
-
 function setBackgroundColor(){
-    let random1 = Math.floor(getRandomInt() * 15);
-    let random2 = Math.floor(getRandomInt() * 15);
-    let random3 = Math.floor(getRandomInt() * 15);
-    colorString = [random1,random2,random3]
-    console.log('Yeah!', colorString);
-    document.getElementById("buttonall").style.backgroundColor = `rgb(${colorString})`;
+    // let random1 = (Math.floor(getRandomInt() * 30) + 15) % 256;
+    // let random2 = (Math.floor(getRandomInt() * 30) + 15) % 256;
+    // let random3 = (Math.floor(getRandomInt() * 30) + 15) % 256;
+    winColorString = [random_item(items),random_item(items),random_item(items)]
+    console.log('Yeah!', winColorString);
+    document.getElementById("buttonall").style.backgroundColor = `rgb(${winColorString})`;
     countDown = 10
     updateCount()
 }
+
+function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [15, 45, 75, 105, 135, 165, 195, 225, 255];
+console.log(random_item(items));
 
 var currentColor = Array (0,0,0);
 
@@ -33,7 +40,7 @@ function changeColor (r,g,b){
         winState()
     } 
 }
-var winCount = 2;
+var winCount = 0;
     
 
 function winState (){
@@ -57,6 +64,27 @@ var countDown = 10;
 
    }
   }
+var rValue = 0;
+var gValue = 0;
+var bValue = 0;
+var currentColor = Array (rValue,gValue,bValue);
+var winColorString = Array (0,0,0);
+
+
+  function setColor(colorString){
+    var rCellValue = colorString.split(",")[0];
+    var gCellValue = colorString.split(",")[1];
+    var bCellValue = colorString.split(",")[2];
+    rValue = parseInt(rCellValue) || rValue ;
+    gValue = parseInt(gCellValue) || gValue ;
+    bValue = parseInt(bCellValue) || bValue ;
+    currentColor = Array (rValue,gValue,bValue);
+    document.body.style.backgroundColor = `rgb(${rValue},${gValue},${bValue})`;
+    console.log(`${rValue},${gValue},${bValue}`);
+    if (currentColor.join() == winColorString.join() ) {
+        winState()
+    } 
+  }
 
    function setCellColor(){
    var babysorry = document.getElementsByClassName("Redsquare")[0];
@@ -67,7 +95,7 @@ var countDown = 10;
     for(i=255; i>0; i=i-30) {
         colorString = [i, gValue, bValue]
         babysorry.children[cellindex].style.backgroundColor = `rgb(${colorString})`;
-        babysorry.children[cellindex].innerHTML = "hi";
+        babysorry.children[cellindex].innerHTML = colorString;
         cellindex = cellindex+1
     console.log("ok");
     }
@@ -82,7 +110,7 @@ function setGreenCellColor(){
      for(i=255; i>0; i=i-30) {
          colorString = [rValue, i, bValue]
          babysorry.children[cellindex].style.backgroundColor = `rgb(${colorString})`;
-         babysorry.children[cellindex].innerHTML = "hi";
+         babysorry.children[cellindex].innerHTML = colorString;
          cellindex = cellindex+1
      console.log("ok");
      }
@@ -97,7 +125,7 @@ function setGreenCellColor(){
      for(i=255; i>0; i=i-30) {
          colorString = [rValue, gValue, i]
          babysorry.children[cellindex].style.backgroundColor = `rgb(${colorString})`;
-         babysorry.children[cellindex].innerHTML = "hi";
+         babysorry.children[cellindex].innerHTML = colorString;
          cellindex = cellindex+1
      console.log("ok");
      }
